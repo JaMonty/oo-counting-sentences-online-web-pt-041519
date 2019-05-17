823478e92270d31fdc5f0457ff24f460ca3cb748
Jason  Montes <jasone.montes@gmail.com>
2019-05-17 00:04:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
   
 def count_sentences
-  array_ct = self.split(/[.?!]).reject { |string| string.empty? }
+  array_ct = self.split(/[.?!]/).reject { |string| string.empty? }
   array_ct.length
 end   
   
